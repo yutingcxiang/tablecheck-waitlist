de3b2d5510699c4cc80ea3f0680026c27dcfea19
Christine Xiang <yutingcxiang@gmail.com>
2024-11-10 09:08:03 -0500

    fix: add additional reservation model validations

--- api/app/models/reservation.rb
+++ api/app/models/reservation.rb
@@ -3,12 +3,13 @@ class Reservation < ApplicationRecord
     after_initialize :add_to_queue
 
     MAX_CAPACITY = 10
+    MIN_CAPACITY = 0
     SERVICE_TIME_PER_PERSON = 3.seconds
     @@WAITLIST = []
     @@AVAILABLE_SEATS = 10
 
-    validates_presence_of :name
-    validates :party_size, numericality: { less_than_or_equal_to: MAX_CAPACITY,  only_integer: true }
+    validates_presence_of :name, allow_empty: false
+    validates :party_size, numericality: { less_than_or_equal_to: MAX_CAPACITY, greater_than: MIN_CAPACITY,  only_integer: true }
 
     def add_to_queue
         @@WAITLIST << self
--- api/spec/models/reservation_spec.rb
+++ api/spec/models/reservation_spec.rb
@@ -17,19 +17,35 @@ RSpec.describe Reservation, type: :model do
     expect(subject).to be_valid
   end
 
-  it "is not valid without a name" do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  context "name" do
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    
+    it "is not valid with an empty string for name" do
+      subject.name = ""
+      expect(subject).to_not be_valid
+    end
   end
 
-  it "is not valid without a party_size" do
-    subject.party_size = nil
-    expect(subject).to_not be_valid
-  end
 
-  it "is not valid for a party_size over 10" do
-    subject.party_size = 20
-    expect(subject).to_not be_valid
+  context "party size" do
+    it "is not valid without a party_size" do
+      subject.party_size = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid for a party_size over 10" do
+      subject.party_size = 20
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid for a party_size under 0" do
+      subject.party_size = -2
+      expect(subject).to_not be_valid
+    end
   end
 
   it "returns the correct capacity" do
