cba574b16e10d503a772fd35668c8a85d3ce6ca2
Christine Xiang <yutingcxiang@gmail.com>
2024-11-09 19:02:54 -0500

    chore: update app name and landing page

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test('renders app title', () => {
+    render(<App />);
+    const linkElement = screen.getByText("Waitlist Manager");
+    expect(linkElement).toBeInTheDocument();
+  });
 });
