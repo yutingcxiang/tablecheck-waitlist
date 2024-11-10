bcd31a2ff3062ff847f6df278bf05eb37562ab5e
Christine Xiang <yutingcxiang@gmail.com>
2024-11-10 07:45:20 -0500

    chore: update docker-compose for migrations

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -6,9 +6,5 @@ rm -f /myApp/tmp/pids/server.pid
 
 bundle install
 
-rake db:create
-rake db:seed
-rake db:migrate
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
